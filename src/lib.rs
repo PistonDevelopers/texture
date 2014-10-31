78fa84c79465148a95f0d3da8f0f91df7a045c32
Sven Nilsen <bvssvni@gmail.com>
2014-10-31 14:19:41 +0100

    Added `FromMemoryAlpha` trait
    Closes https://github.com/PistonDevelopers/texture/issues/6

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 #![deny(missing_doc)]
 
 //! A library for texture conventions.
+//!
+//! Texture trait constructors should pass in a closure like this:
+//!
+//! ```ignore
+//! f: |&mut D, Self| -> Self
+//! ```
+//!
+//! This is a convention such that libraries that construct textures
+//! and store them separately from the texture device,
+//! lets the caller perform additional operations such as mipmap generation.
 
 pub use image_size::ImageSize;
+pub use from_memory_alpha::FromMemoryAlpha;
 
 mod image_size;
+mod from_memory_alpha;
